337a35be96ae618b9503a027a0eb277209681059
Mr.Raindrop <53202747+raindrop213@users.noreply.github.com>
2024-02-02 07:49:44 +0800

    v0.0.1

---
+++ footer.js
@@ -0,0 +1,72 @@
+(function () {
+  var content = `<div id="commonfooter">
+    <div id="main_bottom"></div>
+    <div id="commonfooter_inner">
+      <ul>
+        <li class="head">
+          <h3>プログラム</h3>
+        </li>
+        <li><a href="https://github.com/HIllya51/LunaTranslator/" target="_blank">LunaTranslator</a></li>
+        <li><a href="https://github.com/raindrop213/AnonTranslator/" target="_blank">AnonTranslator</a></li>
+        <li><a href="https://github.com/Artrajz/vits-simple-api" target="_blank">vits-simple-api</a></li>
+        <li><a href="https://foosoft.net/projects/yomichan/" target="_blank">Yomichan</a></li>
+        <li><a href="https://apps.ankiweb.net/" target="_blank">Anki</a></li>
+        <li><a href="https://github.com/kha-white/mokuro/" target="_blank">mokuro</a></li>
+        <li><a href="https://github.com/kha-white/manga-ocr/" target="_blank">manga-ocr</a></li>
+      </ul>
+      <ul>
+      <li class="head">
+        <h3>百合姫オンライン</h3>
+      </li>
+      <li><a href="https://www.ichijinsha.co.jp/yurihime/" target="_blank">コミック百合姫</a></li>
+      <li><a href="https://twitter.com/yh_magazine/" target="_blank">公式twitter</a></li>
+      <li><a href="https://ichijin-plus.com/labels/yurihime/comics/" target="_blank">一迅プラス</a></li>
+      <li><a href="https://www.amazon.co.jp/%E9%9B%91%E8%AA%8C-%E3%82%B3%E3%83%9F%E3%83%83%E3%82%AF%E7%99%BE%E5%90%88%E5%A7%AB-Kindle%E6%9C%AC/s?rh=n%3A2275257051%2Cp_lbr_books_series_browse-bin%3A%E3%82%B3%E3%83%9F%E3%83%83%E3%82%AF%E7%99%BE%E5%90%88%E5%A7%AB" target="_blank">Amazon</a></li>
+         <li><a href="https://bookwalker.jp/label/162/" target="_blank">BOOKWALKER</a></li>
+      </ul>
+      <ul>
+        <li class="head">
+          <h3>インフォメーション</h3>
+        </li>
+        <li><a href="https://yurinavi.com/" target="_blank">百合ナビ</a></li>
+        <li><a href="https://yuriworks.fandom.com/zh/wiki/%E7%99%BE%E5%90%88%E4%BD%9C%E5%93%81_Wiki/"
+            target="_blank">FANDOM WIKI</a></li>
+      </ul>
+      <ul>
+        <li class="head">
+          <h3>評価 & 記録</h3>
+        </li>
+        <li><a href="https://bangumi.tv/" target="_blank">Bangumi</a></li>
+        <li><a href="https://myanimelist.net/" target="_blank">MyAnimeList</a></li>
+        <li><a href="https://bookmeter.com/" target="_blank">読書メーター</a></li>
+      </ul>
+
+      <ul>
+        <li class="head">
+          <h3>リポジトリ</h3>
+        </li>
+        <li><a href="https://github.com/raindrop213/yuriheme-manga-library">Yuri·Hime·Lib</a></li>
+      </ul>
+      <br clear="both">
+
+
+
+      <div style="margin: 0 auto; width:50vw; max-width: 594px; padding:0px;">
+      <div class="circle-avatar" alt="头像" style="display:block;margin: 0 auto;"></div>
+        <br>
+        このプロジェクトは、百合姫公式ホームページ <a href="https://www.ichijinsha.co.jp/yurihime/" target="_blank"> ichijinsha/yurihime </a>
+        を再現しました。このホームページが非常に美しいと感じたので、練習として取り組んでみました。
+        このプロジェクトは<a href="https://github.com/kha-white/mokuro/" target="_blank"> kha-white/mokuro
+        </a>と組み合わされており、漫画はOCR処理が施されており、テキストはすべて選択可能です。美しい本棚の漫画ライブラリーを作り上げました！
+        <br>最後に、このプロジェクトは個人的な学習プロジェクトであり、学習と交流のためにのみ使用されています。商業目的ではなく、すべてのコンテンツの著作権は元の著者と元のウェブサイトに帰属します。
+      </div>
+
+
+      <div id="commoncopyright">
+        ©REBUILT FROM ICHIJINSHA-YURIHIME
+      </div>
+
+    </div>
+  </div>`;
+  document.write(content);
+})();
---
+++ header.js
@@ -0,0 +1,13 @@
+(function(){
+    var content = `<div id="commonheader">
+    <div id="commonheader_inner">
+    <h1><a href="">Yuri·Hime·Lib</a></h1>
+    <ul>
+    <li class="head"><a href="https://www.ichijinsha.co.jp/yurihime/" target="_blank">Origin↗</a></li>
+    <li class="tail"><a href="https://github.com/raindrop213/yuriheme-manga-library" target="_blank">Github↗</a></li>
+    </ul>
+    </div>
+    <div id="commonheadertail"></div>
+    </div>`
+    document.write(content)
+    })();
