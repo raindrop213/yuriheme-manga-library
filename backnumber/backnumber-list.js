aaa7763c90a4e7e2655cde5efc2c27a8c26ed8c1
Mr.Raindrop <53202747+raindrop213@users.noreply.github.com>
2024-04-20 23:55:06 +0800

    基本完善

--- backnumber/backnumber-list.js
+++ backnumber/backnumber-list.js
@@ -5,17 +5,18 @@ fetch("../title/title_structure.json")
     let items = []; // 创建一个空数组来存储所有项
 
     data.forEach((item) => {
+
       // 检查标题是否包含指定字符串，如果不需要这个条件，可以去掉这个if语句
-      if (item.title.includes("コミック百合姫")) {
+      if (item.folderName.includes("コミック百合姫")) {
         item.volumes.forEach((volume) => { // 遍历每个作品的所有卷
-          const path = encodeURIComponent(item.folderName);
-          const imagePath = volume.coverImagePath; // 使用卷的封面图片路径
+          const imagePath = volume.coverImagePath;
+          const path = imagePath.split('/').slice(0, -1).join('/');
           // 将每个项作为字符串添加到数组中
           items.push(`<div class="home-comics__item">
-              <a href="../title/${path}/${volume.volumeName}.html" target="_blank" class="c-cardbox p-link_fade">
-                <span class="c-cardbox__thumb c-cardbox__thumb--shadow p-bgimg p-bgimg--b6 p-bgimg--cover u-mg_b_n" style="background-image:url('../title/${imagePath}')"></span>
+              <a href="../${path}.html" target="_blank" class="c-cardbox p-link_fade">
+                <span class="c-cardbox__thumb c-cardbox__thumb--shadow p-bgimg p-bgimg--b6 p-bgimg--cover u-mg_b_n" style="background-image:url('../${path}.jpg')"></span>
                 <span class="c-cardbox__text">
-                  <span class="c-cardbox__title">${volume.volumeName}</span> <!-- 使用卷名(volumeName) -->
+                  <span class="c-cardbox__title">${volume.volumeName}</span>
                 </span>
               </a>
             </div>
--- title/title-list.js
+++ title/title-list.js
@@ -6,13 +6,12 @@ fetch("title_structure.json")
       '<section class="title-list-list l-grid l-grid--5 l-grid--2-sp l-grid--gap-c_m l-grid--gap-r_l u-mg_b_3l">';
 
     data.forEach((item) => {
-      const path = encodeURIComponent(item.folderName);
       const imagePath = item.volumes[0].coverImagePath;
-      const thumbnailPath = imagePath.split('/').slice(0, -1).join('/') + '.jpg'; // 生成缩略图路径
+      const path = imagePath.split('/').slice(0, -1).join('/');
       htmlContent += `
-            <a href="./${path}/" class="c-cardbox p-link_fade">
+            <a href="../${path}.html" class="c-cardbox p-link_fade">
               <span class="c-cardbox__thumb c-cardbox__thumb--shadow p-bgimg p-bgimg--b6 p-bgimg--cover u-mg_b_n"
-                style="background-image:url('${thumbnailPath}')"></span>
+                style="background-image:url('../${path}.jpg')"></span>
               <span class="c-cardbox__text">
                 <span class="c-cardbox__title">${item.name}</span>
                 <span class="c-cardbox__meta">${item.author}</span>
