5944b385a49517032115ef330c97767c07605b05
Mr.Raindrop <53202747+raindrop213@users.noreply.github.com>
2024-05-20 12:21:01 +0800

    updata

--- header-top.js
+++ header-top.js
@@ -2,6 +2,8 @@
     var content = `<div id="commonheader">
       <div id="commonheader_inner">
         <h1><a href="">Yuri·Hime·Lib</a></h1>
+        <a href="">Yuri·Hime·Lib</a>
+        
         <ul>
           <li class="head"><a href="https://www.ichijinsha.co.jp/yurihime/" target="_blank">Origin↗</a></li>
           <li class="tail"><a href="https://github.com/raindrop213/yuriheme-manga-library" target="_blank">Github↗</a></li>
